21468bb5a14fc5b229e548752ca3020067f1ae86
Kevin Vermeersch <kvermeersch@hooox.com>
2023-01-18 16:10:28 +0100

    Restore default icon

--- dist/vue-treeselect.cjs.js
+++ dist/vue-treeselect.cjs.js
@@ -2578,7 +2578,22 @@ MultiValue_component.options.__file = "src/components/MultiValue.vue"
 var Arrowvue_type_template_id_11186cd4_render = function render() {
   var _vm = this,
     _c = _vm._self._c
-  return _c("div", [_vm._v("[X]")])
+  return _c(
+    "svg",
+    {
+      attrs: {
+        xmlns: "http://www.w3.org/2000/svg",
+        viewBox: "0 0 292.362 292.362",
+      },
+    },
+    [
+      _c("path", {
+        attrs: {
+          d: "M286.935 69.377c-3.614-3.617-7.898-5.424-12.848-5.424H18.274c-4.952 0-9.233 1.807-12.85 5.424C1.807 72.998 0 77.279 0 82.228c0 4.948 1.807 9.229 5.424 12.847l127.907 127.907c3.621 3.617 7.902 5.428 12.85 5.428s9.233-1.811 12.847-5.428L286.935 95.074c3.613-3.617 5.427-7.898 5.427-12.847 0-4.948-1.814-9.229-5.427-12.85z",
+        },
+      }),
+    ]
+  )
 }
 var Arrowvue_type_template_id_11186cd4_staticRenderFns = []
 Arrowvue_type_template_id_11186cd4_render._withStripped = true
@@ -2806,9 +2821,6 @@ var Tip_component = normalizeComponent(
   
 )
 
-/* hot reload */
-if (false) { var Tip_api; }
-Tip_component.options.__file = "src/components/Tip.vue"
 /* harmony default export */ var Tip = (Tip_component.exports);
 // CONCATENATED MODULE: ./node_modules/cache-loader/dist/cjs.js!./node_modules/babel-loader/lib!./node_modules/vue-loader/lib??vue-loader-options!./src/components/Option.vue?vue&type=script&lang=js&
 
